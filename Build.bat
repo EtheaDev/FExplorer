9571ca66c677a4a343cd782af7068ec8d0c391d4
Carlo Barazzetta <carlo.barazzetta@ethea.it>
2021-04-30 20:51:29 +0200

    Fixed Setup: added FExplorer.exe for 32 bit platforms Changed dcu ouptut Folder

--- Build.bat
+++ Build.bat
@@ -2,6 +2,7 @@ call "C:\BDS\Studio\21.0\bin\rsvars.bat"
 msbuild.exe "Source\FExplorer.dproj" /target:Clean;Build /p:Platform=Win64 /p:config=release
 msbuild.exe "Source\FExplorer32.dproj" /target:Clean;Build /p:Platform=Win32 /p:config=release
 msbuild.exe "Source\FEViewer.dproj" /target:Clean;Build /p:Platform=Win64 /p:config=release
+msbuild.exe "Source\FEViewer.dproj" /target:Clean;Build /p:Platform=Win32 /p:config=release
 
 :INNO
 "C:\Program Files (x86)\Inno Setup 6\iscc.exe" "D:\ETHEA\FExplorer\Setup\FExplorer.iss"
